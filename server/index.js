954be0787ae8239b996f1931b0168db5a6a93221
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2025-04-15 15:32:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'https://platform-studynotion.vercel.app/',
+        origin: 'https://platform-studynotion.vercel.app',
         credentials: true,
     })
 )
